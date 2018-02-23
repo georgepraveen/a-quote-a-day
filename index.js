febd335364a42dac7d2df76a5d782158a3612fb7
Praveen George <georgepraveen01@gmail.com>
2018-02-24 03:46:55 +0530

    First uploading

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var  app = express();
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+var port = 3000;
+var server = app.listen(port,function(req,res){
+    console.log("App listening at port"+port);
+});
